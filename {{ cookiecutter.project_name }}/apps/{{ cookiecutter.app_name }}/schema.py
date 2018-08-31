4be0df178639bc78a6e5baf7ceb96982da636cbc
Omar Diaz <oposicioncuba@gmail.com>
2018-08-31 15:23:46 +0200

    Fixing schema

--- {{ cookiecutter.project_name }}/apps/{{ cookiecutter.app_name }}/schema.py
+++ {{ cookiecutter.project_name }}/apps/{{ cookiecutter.app_name }}/schema.py
@@ -1,15 +1,16 @@
 import graphene
+from graphene_django_extras import DjangoObjectType
 
 
-class Queries(graphene.ObjectType):
-    pass
-
-
-class Mutations(graphene.ObjectType):
-    pass
-
-
+# class Queries(graphene.ObjectType):
+#     pass
+#
+#
+# class Mutations(graphene.ObjectType):
+#     pass
+#
+#
 schema = graphene.Schema(
-    query=Queries,
-    mutation=Mutations,
+    # query=Queries,
+    # mutation=Mutations,
 )
