9572737b39412d712a0c3bd1e8326fd846d28d66
Omar Diaz <oposicioncuba@gmail.com>
2018-08-31 15:22:24 +0200

    Fixing schema

--- {{ cookiecutter.project_name }}/apps/{{ cookiecutter.app_name }}/schema.py
+++ {{ cookiecutter.project_name }}/apps/{{ cookiecutter.app_name }}/schema.py
@@ -1,5 +1,14 @@
 import graphene
 
+
+class Queries(graphene.ObjectType):
+    pass
+
+
+class Mutations(graphene.ObjectType):
+    pass
+
+
 schema = graphene.Schema(
     query=Queries,
     mutation=Mutations,
